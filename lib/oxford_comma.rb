2e39892c461b1795897d5661c7433a0e4e618485
Annie Truex <artruex@gmail.com>
2019-05-16 21:16:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
-def oxford_comma(array)
-  if array.size == 1 
-    return array[0]
-  elsif array.size == [2]
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
     return array.join(" and ")
-  else 
-    return array.join(', ') + ", and " + array[-1]
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
   end
 end
